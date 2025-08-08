0dac8697a7cd22446cbe024f73edb79bc172855b
Gabriel Garcia <g4brielgarci4@gmail.com>
2025-08-08 13:11:43 -0300

    Adicionando arquivos iniciais

---
+++ Models/Pacman.py
@@ -0,0 +1,13 @@
+class Pacman():
+    def __init__():
+        self.lives = 3
+        self.direction
+        self.position
+        self.state
+        self.speed
+
+    def move():
+        pass
+
+    def update_state(state):
+        self.state = state
